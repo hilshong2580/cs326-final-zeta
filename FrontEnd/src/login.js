5e2bb7607fd3a5319d9705492f21e8101a6cbc65
miao tingshuo <miaoti@students.santiam.org>
2021-10-22 15:55:28 -0400

    Second update of login

--- FrontEnd/src/login.js
+++ FrontEnd/src/login.js
@@ -1,23 +1,29 @@
-function login() {
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>U-car</title>
  
-    var username = document.getElementById("username");
-    var pass = document.getElementById("password");
+    <link rel="stylesheet" type="text/css" href="login.css"/>
+    <script src="login.js"></script>
+</head>
  
-    if (username.value == "") {
+<body>
+<div id="login_frame">
  
-        alert("username is empty");
+    <p id="image_logo"><img src="https://github.com/hilshong2580/cs326-final-zeta/blob/main/FrontEnd/images/textIcon2.png?raw=true" width="50" alt='U-car Icon'></p>
  
-    } else if (pass.value  == "") {
+    <form method="post" action="login.js">
  
-        alert("password is empty");
+        <p><label class="label_input">Username</label><input type="text" id="username" class="text_field"/></p>
+        <p><label class="label_input">Password</label><input type="text" id="password" class="text_field"/></p>
  
-    } else if(username.value == "..." && pass.value == "..."){
+        <div id="login_control">
+            <input type="button" id="btn_login" value="Login" onclick="login();"/>
+            <a id="forget_pwd" href="forget_pwd.html">Forgrt password?</a>
+        </div>
+    </form>
+</div>
  
-        window.location.href="";
- 
-    } else {
- 
-        alert("Incorrect username or password！")
- 
-    }
-}
+</body>
+</html>
