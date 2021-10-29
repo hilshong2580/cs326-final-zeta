4fbba0a252496f7d61ec26fa91e7d8c193b9bd87
hilshong2580 <63128458+hilshong2580@users.noreply.github.com>
2021-10-29 14:51:12 -0400

    Create a basic endpoint
    Temp to try backend function

---
+++ BackEnd/Shing/client.js
@@ -0,0 +1,18 @@
+const button = document.getElementById('myButton');
+button.addEventListener('click', function(e) {
+  console.log('button was clicked');
+
+  fetch('/clicked', {method: 'GET'})
+  .then(function(response) {
+    if(response.ok) {
+      console.log('Click was recorded');
+      return;
+    }
+    throw new Error('Request failed.');
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+
+});
