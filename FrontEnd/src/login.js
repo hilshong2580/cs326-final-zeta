a64a2376b5f94295bbe634860270b77331e7826f
miao tingshuo <miaoti@students.santiam.org>
2021-11-02 14:00:01 -0400

    Update login.js

--- FrontEnd/src/login.js
+++ FrontEnd/src/login.js
@@ -1,8 +1,24 @@
-function login() {
- 
-    var username = document.getElementById("username");
-    var pass = document.getElementById("password");
- 
+async function addUserInfo(name, word, email,phone,aboutme) {
+    const response = await fetch("http://localhost:3000/users", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({username: name, password: word, email: email,phone:phone,aboutme:aboutme})
+    });
+    if (!response.ok) {
+      console.log(response.error);
+      return;
+    }
+  }
+
+function regis(){
+    let username = document.getElementById("username");
+    let pass = document.getElementById("password");
+    let repass = document.getElementById("repass");
+    let email = document.getElementById("email");
+    let phone = document.getElementById("phone");
+    let aboutme = document.getElementById("aboutme");
     if (username.value == "") {
  
         alert("username is empty");
@@ -10,14 +26,21 @@ function login() {
     } else if (pass.value  == "") {
  
         alert("password is empty");
+    }else if (repass.value  == "") {
  
-    } else if(username.value == "..." && pass.value == "..."){
+        alert("You have to confirm your password");
+    }else if (phone.value  == "") {
  
-        window.location.href="";
+        alert("phone is empty");
+    }else if (aboutme.value  == "") {
  
-    } else {
+        alert("aboutme is empty");
+    }else if (email.value  == "") {
  
-        alert("Incorrect username or password！")
+        alert("email is empty");
+    }else if (pass.value  !== repass.value) {
  
+        alert("Your password does not match");
     }
+    addUserInfo(username.value, pass.value, email.value,phone.value,aboutme.value);
 }
