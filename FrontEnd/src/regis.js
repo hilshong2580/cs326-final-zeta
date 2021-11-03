cd27426a7e1de0f0b0d2f69dc2f2d7d335ebebc6
hilshong2580 <63128458+hilshong2580@users.noreply.github.com>
2021-11-03 18:12:29 -0400

    Update regis.js
    remove regist function

--- FrontEnd/src/regis.js
+++ FrontEnd/src/regis.js
@@ -1,27 +1,22 @@
-
-
-
 document.getElementById("btn_regis").addEventListener("click", function (e) {
   console.log("This is create account: ");
   let emailEnd = "@umass.edu";
-  let username = document.getElementById("usernameRegister").value;
   let password = document.getElementById("passwordRegister").value;
   let repPassword = document.getElementById("repasRegister").value;
   let email = document.getElementById("emailRegister").value;
   let phone = document.getElementById("phoneRegister").value;
   let about = document.getElementById("aboutmeRegister").value;
 
-  if (password === repPassword && email.endsWith(emailEnd)){
-        createAccount({
-      username: username,
+  if (password === repPassword && email.endsWith(emailEnd)) {
+    let name = emailEnd.split("@");
+    createAccount({
+      username: name[0],
       password: password,
       email: email,
       phone: phone,
       about: about,
     });
-  }
-    else
-    alert("password/email incorrect");
+  } else alert("password/email incorrect");
 });
 
 async function createAccount(jsonObj) {
