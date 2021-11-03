433e1c7d94483d3b33b7fc6112f4693da2bf77ca
miao tingshuo <miaoti@students.santiam.org>
2021-11-03 12:56:19 -0400

    Update login.js

--- FrontEnd/src/login.js
+++ FrontEnd/src/login.js
@@ -1,16 +1,56 @@
-async function addUserInfo(name, word, email,phone,aboutme) {
-    const response = await fetch("http://localhost:3000/users", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify({username: name, password: word, email: email,phone:phone,aboutme:aboutme})
-    });
-    if (!response.ok) {
-      console.log(response.error);
-      return;
+async function If_email_ex(email) {
+  let response = await fetch("http://localhost:3000/main/login", {
+    method: "GET",
+  });
+  let sub = email.slice(email.length-10);
+  if(email.length<11 || sub!=='@umass.edu'){
+    return false;
+  }
+  let data = JSON.parse(await response.json());
+  for (let i in data) {
+    if(i.email === email){
+      return false;
     }
   }
+  return true;
+}
+
+async function correct(email,password) {
+  let response = await fetch("http://localhost:3000/main/login", {
+    method: "GET",
+  });
+  let data = JSON.parse(await response.json());
+  for (let i in data) {
+    if(i.email === email && i.password===password){
+      return true;
+    }
+  }
+  return false;
+}
+
+async function addInfo(jsonObj) {
+  fetch("http://localhost:3000/main/registration", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: jsonObj,
+  }).then(async (response) => {
+    const data = await response.text();
+    console.log(data);
+  });
+}
+
+function login(){
+    let pass = document.getElementById("password").value; 
+    let email = document.getElementById("email").value;
+if(correct(email,password)){
+  window.location.href = './main.html';
+}else{
+  alert("Your email or password is wrong, try again!");
+}
+}
+
 
 function regis(){
     let username = document.getElementById("username");
@@ -42,5 +82,16 @@ function regis(){
  
         alert("Your password does not match");
     }
-    addUserInfo(username.value, pass.value, email.value,phone.value,aboutme.value);
+    let info = {
+           email: email.value,
+           username : username.value,
+           password: pass.value,
+           phone : phone.value,
+           aboutme : aboutme.value,
+    }
+    if(If_email_ex(email.value)){
+    addInfo(JSON.stringify(info));
+    }else{
+      alert("Your email has been registrated, please log in");
+    }
 }
