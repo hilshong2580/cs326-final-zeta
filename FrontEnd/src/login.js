56d83a08ceede569a7767a508cbf66e698db730e
hilshong2580 <63128458+hilshong2580@users.noreply.github.com>
2021-11-10 21:03:35 -0500

    Update login.js
    change the url data from email to user's id

--- FrontEnd/src/login.js
+++ FrontEnd/src/login.js
@@ -1,5 +1,5 @@
 document.getElementById("btn_login").addEventListener("click", function (e) {
-  console.log("This is sign in: ");
+  console.log("This is sign in: Button");
   let email = document.getElementById("emailInput").value;
   let password = document.getElementById("passwordInput").value;
   loginAccount({ email: email, password: password });
@@ -15,11 +15,13 @@ async function loginAccount(jsonObj) {
     },
     body: JSON.stringify(jsonObj),
   }).then(async (response) => {
-    const data = await response.text();
-
+    const data = JSON.parse(await response.json());
     if (response.status === 200) {
+      console.log("this is front end login status 200");
+      console.log(data.userid);
       window.location.href =
-        "./main.html?email=" + encodeURIComponent(jsonObj.email);
+         "./main.html?userId=" + encodeURIComponent(data.userid);
     } else alert("username or password incorrect");
   });
 }
+
