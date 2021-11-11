689ad6def6cf6ae9dc0200a59c45ca7b7112fccf
hilshong2580 <63128458+hilshong2580@users.noreply.github.com>
2021-11-10 20:56:55 -0500

    complete the login router
    remove the code from previous mileStone 2, finish the connection between login, registration to database.

--- BackEnd/routes/loginRoute.js
+++ BackEnd/routes/loginRoute.js
@@ -5,7 +5,6 @@ const pool = require("../db");
 //default the static beginning page is login.html on frontend folder
 router.use(express.static("../FrontEnd", { index: "login.html" }));
 
-
 //POST method route to do the registration account function
 //if email already exist return status code 400
 //not exist to return 200 for success
@@ -14,11 +13,12 @@ router.post("/Registration", async function (req, res) {
   try {
     let jsonObj = req.body;
     console.log(req.body);
-    const {password, name, email, phone, about} = req.body;
+    const { password, name, email, phone, about } = req.body;
     const createAccount = await pool.query(
-        "INSERT INTO userTable (password, name, email, phone, about) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-        [password, name, email, phone, about]);
-        res.status(200).send("New Account Create Successful");
+      "INSERT INTO userTable (password, name, email, phone, about) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [password, name, email, phone, about]
+    );
+    res.status(200).send("New Account Create Successful");
   } catch (err) {
     console.log(err.message);
     res.status(400).send("existed");
@@ -33,64 +33,25 @@ router.post("/Account", async function (req, res) {
   console.log("this is login server");
   try {
     //console.log(req.body);
-    const {password, email} = req.body;
-    const loginAccount = await pool.query(   
-        "SELECT * FROM userTable WHERE email = $1 AND password=$2",
-        [email, password]);
+    const { password, email } = req.body;
+    const loginAccount = await pool.query(
+      "SELECT * FROM userTable WHERE email = $1 AND password=$2",
+      [email, password]
+    );
+    //console.log(loginAccount.rows);
 
-        if(loginAccount.rows.length >0)
-          res.status(200).json(loginAccount.rows[0]);
-        else
-          res.status(400).send("Login fail, not match");
+    if (loginAccount.rows.length > 0){
+      console.log("Login successful");
+      console.log(loginAccount.rows);
+      res.status(200).json(JSON.stringify(loginAccount.rows[0]));
+    }
+      
+    else res.status(400).json("Login fail, not match");
   } catch (err) {
     console.log(err.message);
-    res.status(404).send("Login fail");
+    res.status(404).json("Login fail");
   }
 });
 
-// //a function to check the account is exist or not 
-// function signInCheck(email, password) {
-//   if (
-//     accounts.find((data) => data.email === email && data.password === password)
-//   )
-//     return true;
-
-//   return false;
-// }
-
-
-
-// //POST method route to get the user information once login in
-// //it use to display the account information for the user
-// //return status code 200 for success, else return 400
-// router.post("/UserInfo", function (req, res) {
-//   console.log("this is get user info");
-//   let email = req.body.email;
-//   let returnJson = null;
-//   returnJson = getUserInfo(email);
-// console.log(returnJson);
-//   if (returnJson !== null) {
-//     res.status(200).json(JSON.stringify(returnJson));
-//   } else res.status(400).json(JSON.stringify({check:"false"}));
-// });
-
-
-// //a function to find and return the object from json array
-// function getUserInfo(userEmail) {
-//   for (let i in accounts) {
-//     if (JSON.stringify(accounts[i].email) === userEmail) return accounts[i];
-//   }
-//   return null;
-// }
-
-// //a function to check the account exist or not
-// function checkRegist(jsonObj) {
-//   for (let i in accounts) {
-//     console.log(accounts[i].email);
-//   }
-
-//   if (accounts.find((data) => data.email === jsonObj.email)) return false;
-//   return true;
-// }
 
 module.exports = router;
