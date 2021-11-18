d3ecb81bdaffb207452d3657194e9c88e5957f3d
miao tingshuo <miaoti@students.santiam.org>
2021-11-18 14:25:55 -0500

    Update db.js

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -12,21 +12,15 @@ let secrets, user, password, database, host, post;
    host = secrets.host;
    post = secrets.post;
 // } else {
-
-
+ 
 //const secrets = require('./secrets.json');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  }||{
   user: user,
   password: password,
   database: database,
   host: host,
   post: post
-} );
+});
 
 module.exports = pool;
