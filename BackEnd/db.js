f0cd5a03882f432e9dd5e3e29a1171d5988467e0
Wai Kin Yu <jackyyu961215@gmail.com>
2021-11-18 13:41:24 -0500

    Update db.js

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -5,12 +5,18 @@ const { readFileSync, existsSync } = require("fs");
 let secrets, user, password, database, host, post;
 
 if (!process.env.PASSWORD) {
-  secrets = require("./secretsHeroku.json");
-  user = secrets.user;
-  password = secrets.password;
-  database = secrets.database;
-  host = secrets.host;
-  post = secrets.post;
+  //secrets = require("./secretsHeroku.json");
+  // user = secrets.user;
+  // password = secrets.password;
+  // database = secrets.database;
+  // host = secrets.host;
+  // post = secrets.post;
+  user = "kicycnmrgfuhzd";
+  password = "61d7eebb29477caa1646cd54e2996437e16fe6df0825e5ef586b231efba882cb";
+  database = "dfvhtoj672lugq";
+  host = "ec2-107-23-213-65.compute-1.amazonaws.com";
+  post = "5432";
+
 } else {
   user = process.env.NAME;
   password = process.env.PASSWORD;
