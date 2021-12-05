06b48c76f61bdf2ac957b7675cfd3c08b8d3b94f
miao tingshuo <miaoti@students.santiam.org>
2021-12-05 14:01:30 -0500

    Update loginRoute.js

--- BackEnd/routes/loginRoute.js
+++ BackEnd/routes/loginRoute.js
@@ -1,8 +1,9 @@
+const express = require('express');                 // express routing
 const router = express.Router();
 const pool = require("../db");
 require('dotenv').config();
 
-const express = require('express');                 // express routing
+
 const minicrypt = require('../miniCrypt');
 
 const mc = new minicrypt();
