4318de9b90ba42a16a7dc793a1e9f27e780d295e
Fishyu1215 <58120435+Fishyu1215@users.noreply.github.com>
2021-12-08 15:27:11 -0500

    clean up code for myFav.js

--- FrontEnd/src/myFav.js
+++ FrontEnd/src/myFav.js
@@ -2,13 +2,15 @@
 myFavFunction();
 allPostFunction();
 
-let thisUserID=0;
+let thisUserID = 0;
 
 getID();
 
+///////////////////////functions/////////////////////////
 
-function getID(){
-let url = document.location.href,
+//get user ID
+function getID() {
+  let url = document.location.href,
     params = url.split("?")[1].split("&"),
     data = {},
     tmp;
@@ -18,113 +20,74 @@ let url = document.location.href,
   }
   thisUserID = parseInt(data.userId);
 }
-////////////////////////////////////////all post/////////////////////////////////////
-function allPostFunction(){
-    const searchbtr=document.getElementById("allPost");
-    
-    //const input=document.getElementById("searchinput").value;
-    //const searchInput=document.getElementById("searchinput");
-    
-    searchbtr.addEventListener("click", function (e) {
-    
-      //  alert("my FAV");
-      renderall();
-       });
-    
-    }
-    
-    function renderall(){
-        const searchInput=document.getElementById("searchinput");
-        let i=0;
-        while(document.getElementById("post"+JSON.stringify(i))!==null){
-          // console.log(i);
-            i++;
-        }
-    
-    // console.log(document.getElementById("postitle"+JSON.stringify(1)).innerText);
-        
-        for(let j=0;j<i;j++){   
-                document.getElementById("post"+JSON.stringify(j)).hidden= false;    
-        }
-    }
+
+//assign all post button and call show all post function
+function allPostFunction() {
+  const searchbtr = document.getElementById("allPost");
+  searchbtr.addEventListener("click", function (e) {
+    renderall();
+  });
+
+}
+
+//show all post
+function renderall() {
+  const searchInput = document.getElementById("searchinput");
+  let i = 0;
+  while (document.getElementById("post" + JSON.stringify(i)) !== null) {
+    i++;
+  }
+
+  for (let j = 0; j < i; j++) {
+    document.getElementById("post" + JSON.stringify(j)).hidden = false;
+  }
+}
 
 
 
 /////////////////////////////filter/////////////////////////////
 
-
-function myFavFunction(){
-const searchbtr=document.getElementById("MyFavPost");
-
-//const input=document.getElementById("searchinput").value;
-//const searchInput=document.getElementById("searchinput");
-
-searchbtr.addEventListener("click", function (e) {
-
-    // alert(thisUserID);
+//hide all post first, then show the post that is set with user favour
+function myFavFunction() {
+  const searchbtr = document.getElementById("MyFavPost");
+  searchbtr.addEventListener("click", function (e) {
     hideAll();
     getUserFav();
-   });
+  });
 
 }
 
-
+//connect end-point and get data of all favour post wiht specific user id
 async function getUserFav() {
-    console.log("this is get user fav with id:" + thisUserID);
-    fetch("/main/UserFav", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userId: thisUserID }),
-    }).then(async (response) => {
-      const data = JSON.parse(await response.json());
-      if (response.status === 200) {
-         // let tempString=JSON.stringify(data);
-         // tempString.replace("userid\":1", "");
-        //  alert(tempString);
-       
-
-          for (let i in data) {  
-           // console.log(data.postId);
-            let dataTemp = {
-              postId: data[i].postid,
-              postTag: data[i].posttag
-            };
-        console.log(dataTemp.postTag);
-/////////////////////
-        // const searchInput=document.getElementById("searchinput");
-        // let l=0;
-        // while(document.getElementById("post"+JSON.stringify(l))!==null){
-        // // console.log(i);
-        //     i++;
-        // }
-        // console.log(document.getElementById("postitle"+JSON.stringify(1)).innerText);
-       // for(let j=0;j<i;j++){
-           // if(((document.getElementById("postitle"+JSON.stringify(j)).innerText).toLowerCase())===dataTemp.postId){ 
-            document.getElementById(dataTemp.postTag).hidden=false;
-            //console.log(document.getElementById("pid"+ JSON.stringify(dataTemp.postId)).tag);
-              //  }
-          //  }
-
-
-        ///////////////////////
-
-        }
+  fetch("/main/UserFav", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userId: thisUserID }),
+  }).then(async (response) => {
+    const data = JSON.parse(await response.json());
+    if (response.status === 200) {
+      for (let i in data) {
+        let dataTemp = {
+          postId: data[i].postid,
+          postTag: data[i].posttag
+        };
+        document.getElementById(dataTemp.postTag).hidden = false;
       }
-    });
-  }
-  
+    }
+  });
+}
 
-function hideAll(){
- 
-    const searchInput=document.getElementById("searchinput");
-    let i=0;
-    while(document.getElementById("post"+JSON.stringify(i))!==null){
-      // console.log(i);
-        i++;
-    }
-    for(let j=0;j<i;j++){
-            document.getElementById("post"+JSON.stringify(j)).hidden= true;
-    }
+//hide all post
+function hideAll() {
+
+  const searchInput = document.getElementById("searchinput");
+  let i = 0;
+  while (document.getElementById("post" + JSON.stringify(i)) !== null) {
+    i++;
+  }
+  for (let j = 0; j < i; j++) {
+    document.getElementById("post" + JSON.stringify(j)).hidden = true;
+  }
 }
